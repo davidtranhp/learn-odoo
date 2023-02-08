ebc5600133eb288c0065f5c2e18e8189b183fad1
David Tran <david.tran@tvtmarine.com>
2023-02-08 07:54:44 +0700

    add website

--- viindoo_academy/controllers/controllers.py
+++ viindoo_academy/controllers/controllers.py
@@ -2,10 +2,20 @@ from odoo import http
 
 
 class Main(http.Controller):
-    
-    @http.route('/academy/academy', auth='public')
+
+    @http.route('/academy/academy', auth='public', website=True)
     def index(self, **kwargs):
         Teachers = http.request.env['academy.teachers']
-        return http.request.render('viindoo_academy.index', {
-            'teachers': Teachers.sudo().search([])
-            })
+        
+        return http.request.render(
+            'viindoo_academy.index',
+            {
+                'teachers': Teachers.search([])
+                }
+            )
+
+    @http.route('/academy/<model("academy.teachers"):teacher>/', auth='public', website=True)
+    def teacher3(self, teacher):
+        return http.request.render('viindoo_academy.biography', {
+            'person': teacher
+        })
--- viindoo_academy/models/academy_teachers.py
+++ viindoo_academy/models/academy_teachers.py
@@ -5,5 +5,21 @@ class AcademyTeachers(models.Model):
     _name = 'academy.teachers'
 
     name = fields.Char()
-
+    birthday = fields.Date(
+        string="Date of birth"
+        )
+    age = fields.Integer(
+        string='Age',
+        compute='_compute_teacher_age'
+        )
+    biography = fields.Html()
+    
+    @api.depends('birthday')
+    def _compute_teacher_age(self):
+        today = fields.Date.today()
+        for r in self:
+            if r.birthday:
+                r.age = today.year - r.birthday.year
+            else:
+                r.age = False
 
